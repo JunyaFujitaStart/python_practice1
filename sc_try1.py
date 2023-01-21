fd0f485f6f21bed28e5f6e16213cac8bc0125d78
藤田　順也 <fujitajunya@fujitajunyanoMacBook-Pro.local>
2023-01-21 11:26:11 +0900

    3

--- sc_try1.py
+++ sc_try1.py
@@ -2,17 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 
 # Webページを取得して解析する
-load_url = "https://www.ymori.com/books/python2nen/test2.html"
+load_url = "https://dq10ragu.com/"
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
-
+print(soup)
 # すべてのliタグを検索して、その文字列を表示する
-for element in soup.find_all("li"):    # すべてのliタグを検索して表示
-    print(element.text)
+# for element in soup.find_all("li"):    # すべてのliタグを検索して表示
+#     print(element.text)
 
-print('ここで区切り')
+# print('ここで区切り')
 # IDで検索して、そのタグの中身を表示する
-chap2 = soup.find(id="chap2")    # idが「chap2」の範囲の要素を表示
+chap2 = soup.find(class_="clearfix")    # idが「chap2」の範囲の要素を表示
+# for chap3 in chap2.find_all("h3"): # すべてのliタグを検索して表示
+#     chap4=chap3.get('href')
 print(chap2)
 #
 # 要素 = soup.find(class_="class名")
